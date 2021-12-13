3d8ff55a7811634c4988bfdedc2665093931e580
bengil <benjyg@gmail.com>
2021-12-13 19:24:01 +0200

    update tailwind transformer to use jsonpath

--- styling/tailwind/webpack-transformer/tailwind.transformer.ts
+++ styling/tailwind/webpack-transformer/tailwind.transformer.ts
@@ -1,70 +1,81 @@
 import { WebpackConfigTransformer, WebpackConfigMutator } from "@teambit/webpack";
-import * as stylesRegexps from "@teambit/webpack.modules.style-regexps";
+import { inspect } from "util";
+import { JSONPath } from "jsonpath-plus";
 import tailwindcssPlugin from "tailwindcss";
-// import { tailwindConfigPath } from "@shohamgilad/tailwind-test.styles.tailwind-styles";
 
 export type TailwindTransformersType = {
     previewConfigTransformer: WebpackConfigTransformer,
     devServerConfigTransformer: WebpackConfigTransformer
 }
 
+type PostcssOptions = { plugins?: any[] };
+
 export function UseTailwindTransformer(tailwindConfigPath: string): TailwindTransformersType {
 
-    function addTailwindConfig(config: WebpackConfigMutator): WebpackConfigMutator {
-        // @ts-ignore
-        const oneOfRule = findOneOfRuleInPreviewConfig(config.raw.module.rules);
-        const cssRule = findCssRuleByCssNoModuleRegexp(oneOfRule);
-        if (!cssRule) {
-          throw new Error(
-            "css rule not found. this probably means the webpack config of bit itself has changed"
-          );
-        }
-        // we already have a postcss loader
-        const postcssLoader = findPostcssLoaderInRule(cssRule.use);
-        if (!postcssLoader) {
-          throw new Error(
-            "postcss loader not found. this probably means the webpack config of bit itself has changed"
-          );
-        }
-        postcssLoader.options.postcssOptions.plugins.unshift(
-          tailwindcssPlugin(tailwindConfigPath)
-        );
-        return config;
-      }
+  function addTailwindConfig(config: WebpackConfigMutator): WebpackConfigMutator {
+    
+    const postcssLoaders = JSONPath<any[]>({
+        json: config.raw,
+        path: "$.module.rules..[?(@ && @.loader && @.loader.includes('postcss-loader'))]",
+    });
+    const postcssOptions = JSONPath<any[]>({
+        json: postcssLoaders,
+        path: "$..postcssOptions",
+    });
+    
+    validatePostcss(postcssLoaders, postcssOptions);
 
-     const previewConfig: WebpackConfigTransformer = (
-        config: WebpackConfigMutator
-      ) => {
-        return addTailwindConfig(config);
-      };
-      
-     const devServerConfig: WebpackConfigTransformer = (
-        config: WebpackConfigMutator
-      ) => {
-        return addTailwindConfig(config);
-      };
+    postcssOptions.forEach(options => {
+      validateOnePostCssOptions(options);
+      if (!options.plugins) options.plugins = [];
+      const plugin = tailwindcssPlugin(tailwindConfigPath);
+      options.plugins.unshift(plugin);
+    });
+    return config;
+  }
 
-    return {
-        previewConfigTransformer: previewConfig,
-        devServerConfigTransformer: devServerConfig
-    }
+  const previewConfig: WebpackConfigTransformer = (
+    config: WebpackConfigMutator
+  ) => {
+    return addTailwindConfig(config);
+  };
+  
+  const devServerConfig: WebpackConfigTransformer = (
+    config: WebpackConfigMutator
+  ) => {
+    return addTailwindConfig(config);
+  };
+
+  return {
+      previewConfigTransformer: previewConfig,
+      devServerConfigTransformer: devServerConfig
+  }
 }
 
-
-
-
-
-function findCssRuleByCssNoModuleRegexp(rules: Array<any> = []) {
-  return rules.find(
-    (rule) => rule.test.toString() === stylesRegexps.cssNoModulesRegex.toString()
-  );
+function validatePostcss(loaders: any[], options: any[]){
+  if (loaders.length > options.length) {
+    throw new Error(
+      `tailwind-bit env: failed to add tailwind to all postcss options.` +
+      ` Found ${loaders.length} postcss loaders, but only ${options.length} postcss options.` +
+      ` This probably means the webpack configuration of Bit itself has changed!`
+    );
+  }
+  if (options.length < 1)
+    throw new Error(
+      `tailwind-bit env: failed to add tailwind postcss options - found no postcss options or loaders` +
+      ` This probably means the webpack configuration of Bit itself has changed!`
+    );
 }
 
-function findOneOfRuleInPreviewConfig(rules: Array<any> = []) {
-  const rule = rules.find((rule) => !!rule.oneOf);
-  return rule.oneOf;
+function isPostcssOptions(obj: any): obj is PostcssOptions {
+  return !!obj && (obj.plugins === undefined || Array.isArray(obj.plugins));
 }
 
-function findPostcssLoaderInRule(loaders: Array<any>) {
-  return loaders.find(loader => loader.loader.includes('postcss'));
+function validateOnePostCssOptions(options: any) {
+  if (!isPostcssOptions(options)) {
+    const stringified = inspect(options, undefined, 5);
+    throw new Error(
+        `tailwind-bit env: failed to add tailwind plugin because this postcss options object is malformed: ${stringified}`
+    );
+}
 }
