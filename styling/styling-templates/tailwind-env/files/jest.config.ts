74f9939abefbfd73d49f2a4441323471971791a6
bengil <benjyg@gmail.com>
2022-07-19 10:13:54 +0300

    fit lit template jest config

--- styling/styling-templates/tailwind-env/files/jest.config.ts
+++ styling/styling-templates/tailwind-env/files/jest.config.ts
@@ -3,7 +3,7 @@
 export function jestConfigFile() {
   return `// Override the Jest config to ignore transpiling from specific folders
 // import { generateNodeModulesPattern } from '@teambit/dependencies.modules.packages-excluder';
-// const litJestConfig = require('@teambit/web-components.lit/jest/jest.config');
+// const jestConfig = require('@teambit/react/jest/jest.config');
 
 // const packagesToTransform = [
 //  // add packages you want jest to transpile here. E.g. @bitOrgName so that all your bit components will be transpiled in your tests
@@ -12,7 +12,7 @@ export function jestConfigFile() {
 // const transformIgnorePatterns = generateNodeModulesPattern({packages: packagesToTransform});;
 
 // module.exports = {
-//   transform: litJestConfig.transform,
+//   transform: jestConfig.transform,
 //   transformIgnorePatterns: [transformIgnorePatterns],
 // };
 `;
